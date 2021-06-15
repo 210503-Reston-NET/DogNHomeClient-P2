066264f3a4794df58697e1a0f175b3d5f40e97d9
Robbie <rfishe22@students.kennesaw.edu>
2021-06-15 15:25:07 -0400

    Pushing environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyAKJhvm4YPHfKf1XOQeJhm15EfNPPweUcQ",
+    authDomain: "dognhome-2eaaa.firebaseapp.com",
+    databaseURL: "dognhome-2eaaa",
+    projectId: "dognhome-2eaaa.appspot.com",
+    storageBucket: "dognhome-2eaaa.appspot.com",
+    messagingSenderId: "422565600673",
+    appId: "1:422565600673:web:ce414322bc7bccc8a23fe0"
+  },
+  petFinderCeds: {
+    grant_type: "client_credentials",
+    client_id: "uPVhhqS9BsIfUwejLjZVUhQkzXniBO7I8RZ41YUCwidgk71ZZa",
+    client_secret: "DoIXB7bl3o4D87yCDOw4mOGLh08TxPsSjLYZrgIe"
+  }
+}
